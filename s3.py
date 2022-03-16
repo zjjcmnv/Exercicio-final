77fdac9efb818e3a866d5bb36bdf1f8e572de5c8
Ubuntu <ubuntu@ip-172-31-95-113.ec2.internal>
2022-03-16 01:01:10 +0000

    Adicionado insercao sem logica no dynamodb

--- s3.py
+++ s3.py
@@ -1,4 +1,21 @@
 import json
+import boto3
+
+s3_client = boto3.client('s3')
+dynamodb_client = boto3.resource('dynamodb')
+table = dynamodb_client.Table('resultado_votos')
 
 def handler(event, context):
-    print(json.dumps(event))
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    file = event['Records'][0]['s3']['object']['key']
+    json_object = s3_client.get_object(Bucket=bucket,Key=file)
+    json_file = json_object['Body'].read()
+    conteudo = json.loads(json_file)
+    votos = conteudo['votos']
+    total = 0 
+    for item in votos:
+        total += votos[item]
+        table.put_item(Item={'candidato': item,'total': votos[item]})
+        print('Item inserido do dynamodb = '+item+', total'+str(votos[item]))
+    table.put_item(Item={'candidato': 'totaldevotos','total': total})
+    
